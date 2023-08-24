1d37d3aeb3060ae15647ddf2bb562347925ab961
dchernyavskiy <99835818+dchernyavskiy@users.noreply.github.com>
2023-08-24 09:46:35 +0300

    minor changes

--- src/WebUI/Controllers/AboutController.cs
+++ src/WebUI/Controllers/AboutController.cs
@@ -8,25 +8,6 @@ public class AboutController : Controller
     [HttpGet]
     public IActionResult Index()
     {
-        return View();
-    }
-}
-
-public class AboutModel : PageModel
-{
-    [BindProperty]
-    public string Description { get; set; } = "Default description of the algorithm.";
-
-    public void OnGet()
-    {
-        // You can fetch the description from a database or a configuration file here
-    }
-
-    public void OnPost()
-    {
-        if (User.Identity.IsAuthenticated && User.IsInRole("Admin"))
-        {
-            // Save the edited description to the database or configuration file
-        }
+        return View(new { Description = "Description" });
     }
 }
